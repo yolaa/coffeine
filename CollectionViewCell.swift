033b54639268e4d59be8f559bd072ad311f56f57
Dimitri Yola <68340584+yolaa@users.noreply.github.com>
2024-08-06 19:35:57 +0700

    add swift files
    add files via upload 2nd directory, out of folder

---
+++ CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  Coffeine
+//
+//  Created by Dimitri Yola on 06/05/21.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var coffeImageView: UIImageView!
+    @IBOutlet weak var coffeLabel: UILabel!
+    
+}
